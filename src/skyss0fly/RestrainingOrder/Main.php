552ac58710e1dffbda9b18637e3a2d5a759c6558
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-03-23 01:52:04 +1000

    Create Main.php

---
+++ src/skyss0fly/RestrainingOrder/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace skyss0fly\RestrainingOrder;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Player\PlayerChatEvent;
+class Main {
+
+  public function onChat(PlayerChatEvent $event){
+$player = $event->getPlayer();
+
+    if ($player->getName() === "Russiandream817"); {
+
+    }
+    else {
+
+    }
+  }
+}
